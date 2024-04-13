86db3bf1dd0e7eaa0726afceee1c6781118254f2
Katsiaryna Tymoshchyk <solny6ko.net@gmail.com>
2024-04-13 11:06:05 +0300

    Day 16 - Classwork - First WebDriver tasks

---
+++ src/test/java/classwork/day16/Day16Driver.java
@@ -0,0 +1,22 @@
+package classwork.day16;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Day16Driver {
+
+    public static void main(String[] args) throws MalformedURLException {
+//        Запускает Вебдрайвер локально
+//        WebDriver driver = new ChromeDriver();
+//        Запускает Вебдрайвер на удаленном компьютере (конструктор, который принимает на вход два объекта)
+        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:9515"), new ChromeOptions());
+        driver.get("https://selenium.dev");
+//        (ChromeDriver)driver.findElement(); - при явном приведении типов будет больше методов
+//        у ChromeDriver, чем у driver
+    }
+}
