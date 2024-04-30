b55a091579ab2091be61adf9315e3764bb77d414
Katsiaryna Tymoshchyk <solny6ko.net@gmail.com>
2024-04-30 18:43:06 +0300

    Rework, second test started

--- src/test/java/steps/BookingBddSteps.java
+++ src/test/java/steps/BookingBddSteps.java
@@ -7,8 +7,9 @@ import io.cucumber.java.en.When;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
 import pages.booking.BookingHomePageXPath;
+import tests.BaseStepsJUnit;
 
-public class BookingBddSteps {
+public class BookingBddSteps extends BaseStepsJUnit {
     private static final Logger LOGGER = (Logger) LogManager.getLogger(BookingBddSteps.class);
     private BookingHomePageXPath bookingHomePage = new BookingHomePageXPath();
 
@@ -40,6 +41,19 @@ public class BookingBddSteps {
     public void checkCheckAltTextCurrency(String AltTextCurrencyValue){
         bookingHomePage.checkCheckAltTextCurrency("Select your currency");
         LOGGER.info("Tooltip is 'Select your currency'");
+
+    }
+
+    @When("I hover mouse on language button")
+    public void hoverMouseOnLanguageBtn(){
+        bookingHomePage.hoverMouseOnLanguageBtn();
+        LOGGER.info("I hovered over Language Button");
+    }
+
+    @Then("I see currency tooltip message {string} of currency button")
+    public void checkCheckAltTextLanguage(String AltTextLanguageValue){
+        bookingHomePage.checkCheckAltTextLanguage("Select your language");
+        LOGGER.info("Tooltip is 'Select your language'");
 
     }
 
--- src/test/resources/features/firstfeature/bookingBdd.feature
+++ src/test/resources/features/firstfeature/bookingBdd.feature
@@ -1,7 +1,14 @@
 Feature: My Booking bdd scenarios
-  Scenario: Booking hover test
+  Scenario: Booking hover Currency test
     Given I open booking homepage
     And I dismiss sign in
     And I accept cookies
     When I hover mouse on currency button
     Then I see currency tooltip message "Select your currency" of currency button
+
+  Scenario: Booking hover Language test
+    Given I open booking homepage
+    And I dismiss sign in
+    And I accept cookies
+    When I hover mouse on language button
+    Then I see language tooltip message "Select your language" of currency button
