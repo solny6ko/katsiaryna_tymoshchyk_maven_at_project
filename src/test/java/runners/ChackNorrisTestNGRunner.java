3bf50e8ccd61f6b120e48d56bdca936b551a31f4
Katsiaryna Tymoshchyk <solny6ko.net@gmail.com>
2024-05-05 15:27:10 +0300

    All tasks for Chuck Norris are resolved

---
+++ src/test/java/runners/ChackNorrisTestNGRunner.java
@@ -0,0 +1,19 @@
+package runners;
+
+import org.testng.TestNG;
+import org.testng.xml.XmlSuite;
+import org.testng.xml.internal.Parser;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ChackNorrisTestNGRunner {
+    public static void main(String[] args) throws IOException {
+        final TestNG testNG = new TestNG(true);
+        final Parser parser = new Parser("testng.xml");
+        final List<XmlSuite> suites = parser.parseToList();
+        testNG.setXmlSuites(suites);
+        testNG.run();
+        System.out.println(System.getProperty("CONFIG"));
+    }
+}
--- src/test/java/tests/TestNGTests/ChuckNorrisRequests.java
+++ src/test/java/tests/TestNGTests/ChuckNorrisRequests.java
@@ -8,29 +8,71 @@ import io.restassured.specification.RequestSpecification;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
 import org.testng.annotations.Test;
-import tests.BaseStepsTestNG;
+
+
+import java.util.Random;
 
 public class ChuckNorrisRequests {
     private static final Logger LOGGER = (Logger) LogManager.getLogger(ChuckNorrisRequests.class);
 
-        RequestSpecification requestSpec = new RequestSpecBuilder()
-                .setBaseUri("https://api.chucknorris.io/jokes/random")
-                .setAccept(ContentType.JSON)
-                .setContentType(ContentType.JSON)
-                .log(LogDetail.ALL)
-                .build();
+    RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("https://api.chucknorris.io/jokes")
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
 
-        @Test
-                public void randomJoke(){
-            String chuckNorrisRandomJoke = RestAssured
-                    .given()
-                    .spec(requestSpec)
-                    .when()
-                    .get()
-                    .then()
-                    .extract().body().asString();
-            LOGGER.info(chuckNorrisRandomJoke);
-        }
+    @Test
+    public void randomJoke() {
+        String chuckNorrisRandomJoke = RestAssured
+                .given()
+                .spec(requestSpec)
+                .when()
+                .get("/random")
+                .then()
+                .extract().body().asString();
+        LOGGER.info(chuckNorrisRandomJoke);
+    }
 
+    @Test
+    public void jokesCategoriesList() {
+        String chuckNorrisJokesCategoriesList = RestAssured
+                .given()
+                .spec(requestSpec)
+                .when()
+                .get("/categories")
+                .then()
+                .extract().body().asString();
+        LOGGER.info(chuckNorrisJokesCategoriesList);
+        int chuckNorrisJokesCategoriesListLength = chuckNorrisJokesCategoriesList.length();
 
+        String convert1 = chuckNorrisJokesCategoriesList.substring(2, chuckNorrisJokesCategoriesListLength - 2);
+
+        String[] jokeCategories = convert1.split("\",\"");
+        Random rand = new Random();
+        int n = rand.nextInt(jokeCategories.length);
+        String category = jokeCategories[n];
+
+        String chuckNorrisJokeFromRandomCategory = RestAssured
+                .given()
+                .spec(requestSpec)
+                .param("category", category)
+                .when()
+                .get("/random")
+                .then()
+                .extract().body().asString();
+        LOGGER.info(chuckNorrisJokeFromRandomCategory);
+    }
+    @Test
+    public void freeTextSearch() {
+        String freeTextSearchForJoke = RestAssured
+                .given()
+                .spec(requestSpec)
+                .param("query", "shoe")
+                .when()
+                .get("/search")
+                .then()
+                .extract().body().asString();
+        LOGGER.info(freeTextSearchForJoke);
+    }
 }
